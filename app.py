c6d720d15e4a9f516f409901ecb5adadc8c0ffbc
premrasapalli <premrasapalli82@gmail.com>
2025-11-01 23:10:07 +0530

    deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 RASAPALLI PREM IS GREATEST MAN IN THE WORLD....."
+    return "🚀 RASAPALLI PREM IS GREATEST MAN IN THE WORLD......................"
 
 if __name__ == '__master__':
     app.run(host='0.0.0.0', port=5000)
