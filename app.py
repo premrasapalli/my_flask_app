2521cb30a15a4f0b7378056fe303743824ec0bbf
premrasapalli <premrasapalli82@gmail.com>
2025-11-01 22:32:28 +0530

    prem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Flask App Running via System IP and CI/CD"
+    return "🚀 RASAPALLI PREM GREATEST MAN IN THE WORLD"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
